ef0a8c2e9316b7e9c8e306d11fc3a4c64a2249f1
O direct do mato <69155843+Directinho@users.noreply.github.com>
2022-02-12 03:31:44 -0300

    play

--- Music/YoutubePlayer.js
+++ Music/YoutubePlayer.js
@@ -71,6 +71,39 @@ client.on("message", msg => {
         });
     }
 });
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const commands = [{
+    name: "play",
+    description: "Começar a musica! :3",
+    options: [
+        {
+            name: "query",
+            type: "STRING",
+            description: "Pesquise",
+            required: true
+        }
+    ]
+}]; 
+
+const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application [/] commands.");
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log("Successfully reloaded application [/] commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 
 client.login("TOKEN");
-Unkn
+
