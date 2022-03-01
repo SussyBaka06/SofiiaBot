e68603902c33bfaab99d72d7eafdb22ba43b203e
DirectDev <jcmassoco2@gmail.com>
2022-03-01 04:38:51 -0300

    main index

---
+++ index.js
@@ -0,0 +1,5 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const express = require('express');
+const config = require('./config.json');
