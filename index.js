5d482252ac5e18ff32dbf91c617196ecccd1bac1
DirectDev <jcmassoco2@gmail.com>
2022-03-01 05:28:45 -0300

    index update

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const { Routes } = require('discord-api-types/v9');
 const { token } = require('./config.json');
 const express = require('express');
 const config = require('./config.json');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Starting!')
+});
+
+app.listen(3000, () => {
+  console.log('server started :3');
+});
